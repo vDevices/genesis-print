08bd6365dc230bec7105d0d70098d7411d4de853
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-04 09:50:50 +0000

    tagging 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/genesis-print/tags/1.1.2@433165 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ genesis-print.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Genesis Print Style
+Plugin URI: http://www.ramoonus.nl/wordpress/genesis-print/
+Description: This plugin adds a printer-ready CSS file for the Genesis Theme Framework.
+Version: 1.1.2
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/
+*/
+/* CSS */ 
+function genesis_print() {
+	//wp_enqueue_style('genesis', get_bloginfo('template_directory').'/style.css', false, '1.1.2', 'print');
+	wp_enqueue_style('genesis-print-style', plugins_url('/css/print.css', __FILE__), false, '1.1.2', 'print');
+}
+add_action('init', 'genesis_print');
+?>
